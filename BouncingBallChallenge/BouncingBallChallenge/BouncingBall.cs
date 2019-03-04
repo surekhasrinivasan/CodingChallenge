0bf5b5d4a586bff25e2f4071716815c613b715b6
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-04 12:47:22 -0500

    BouncingBallChallenge - completed

---
+++ BouncingBallChallenge/BouncingBallChallenge/BouncingBall.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBallChallenge
+{
+    public class BouncingBall
+    {
+        /*Take note of the following conditions:
+
+        ** Float parameter "h" in meters must be greater than 0**
+        ** Float parameter “br" must be greater than 0 and less than 1**
+        ** Float parameter “win" must be less than h.**
+        * If all three conditions above are fulfilled, return a positive integer of the number of times the mother sees the ball pass her window, otherwise return -1.
+        * 
+        * Note: The ball can only be seen if the height of the rebounding ball is stricty greater than the win parameter. IE: rebounding h > win
+
+         */
+        public int bouncingBall(double height, double bounceRate, double window)
+        {
+            // ballSeen is -1 if any of the inputs are not valid 
+            int ballSeen = -1;
+            if(height > 0 && bounceRate > 0 && bounceRate < 1)
+            {
+                while(height > window)
+                {
+                    height *= bounceRate;
+                    ballSeen +=2;
+                }
+            }            
+            return ballSeen;
+        }
+    }
+}
---
+++ BouncingBallChallenge/BouncingBallChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBallChallenge
+{
+    class Program
+    {
+        /* A child is playing with a ball on the nth floor of a tall building. The height of this floor, is h. For example 3 meters
+         * He drops the ball out of the window. The ball bounces at a rate of br of it’s height for example a rate of 
+         * two-thirds of its height would be a bounce rate of 0.66.
+         * His mother looks out of a window win meters from the ground. Example: 1.5 meters
+         * How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+         */
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the height of the floor: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the ball bounce rate: ");
+            double br = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the window height from the ground: ");
+            double w = Convert.ToDouble(Console.ReadLine());
+
+            BouncingBall b = new BouncingBall();
+
+            Console.WriteLine(b.bouncingBall(h, br, w));            
+        }
+    }
+}
