e52f9679a39eaa0832ce745d7639b9715582fece
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-11 11:38:35 -0400

    LinkedListChallenge - initial commit

---
+++ LinkedListChallenge/LinkedListChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListChallenge
+{
+    // Definition for singly-linked list:
+    // class ListNode<T> {
+    //   public T value { get; set; }
+    //   public ListNode<T> next { get; set; }
+    // }
+    //
+
+    /* Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k
+     * For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
+     * removeKFromList(l, k) = [1, 2, 4, 5];
+     * For l = [1, 2, 3, 4, 5, 6, 7] and k = 10, the output should be
+     * removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
