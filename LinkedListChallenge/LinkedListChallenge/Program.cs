fd0d74e2e68a3348ad7dbc6424c2aef14a279de6
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-13 20:24:56 -0400

    removeKfromlist method completed

--- LinkedListChallenge/LinkedListChallenge/Program.cs
+++ LinkedListChallenge/LinkedListChallenge/Program.cs
@@ -7,12 +7,44 @@ using System.Threading.Tasks;
 namespace LinkedListChallenge
 {
     // Definition for singly-linked list:
-    // class ListNode<T> {
-    //   public T value { get; set; }
-    //   public ListNode<T> next { get; set; }
-    // }
-    //
+    public class ListNode<T>
+    {
+        public T value { get; set; }
+        public ListNode<T> next { get; set; }
+    
+    public static ListNode<int> removeKFromList(ListNode<int> l, int k)
+        {
+            // if the list is emp
+            if (l == null)
+                return l;
+            else if (l.value == k)
+            {
+                l = l.next;
+            }
 
+            // temproray ListNode variable traverse through the list
+            ListNode<int> temp = l;
+
+            while (temp != null && temp.next != null)
+            {
+                if (temp.next.value == k)
+                {
+                    temp.next = temp.next.next;
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+            }
+            if (temp != null && temp.value == k)
+            {
+                l = l.next;
+            }
+            return l;
+        }
+    }      
+
+    
     /* Given a singly linked list of integers l and an integer k, remove all elements from list l that have a value equal to k
      * For l = [3, 1, 2, 3, 4, 5] and k = 3, the output should be
      * removeKFromList(l, k) = [1, 2, 4, 5];
@@ -20,9 +52,10 @@ namespace LinkedListChallenge
      * removeKFromList(l, k) = [1, 2, 3, 4, 5, 6, 7].
      */
     class Program
-    {
+    {       
         static void Main(string[] args)
-        {
+        {            
         }
     }
+    
 }
