1d3d2ebb8cf1fc6cfc5030371989ef1b4671e269
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-15 12:17:34 -0400

    checkPalindrome - initial commit

---
+++ ArcadeChallenges/checkPalindrome/checkPalindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkPalindrome
+{
+    /* Given the string, check if it is a palindrome.
+     * 
+     * Example :
+     * For inputString = "aabaa", the output should be
+     * checkPalindrome(inputString) = true;
+     * For inputString = "abac", the output should be
+     * checkPalindrome(inputString) = false;
+     * For inputString = "a", the output should be
+     * checkPalindrome(inputString) = true.
+     * 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
