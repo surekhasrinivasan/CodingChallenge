561fabe4eb64dd6f10511894d0ae652288e2095a
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-16 11:56:10 -0400

    IntegerReverseOrder challenge - Solved - refactored code

--- IntegerReverseOrder/IntegerReverseOrder/ReverseOrderKata.cs
+++ IntegerReverseOrder/IntegerReverseOrder/ReverseOrderKata.cs
@@ -10,19 +10,14 @@ namespace IntegerReverseOrder
     {
         public static int ReversedOrder(int num)
         {
-            // write the code here
             int reverse = 0;
 
             while (num > 0)
             {
                 int remainder = num % 10;
-                Console.WriteLine("remainder: " + remainder);
                 reverse = (reverse * 10) + remainder;
-                Console.WriteLine("reverse: " +reverse);
                 num = num / 10;
-                Console.WriteLine("num: " +num);
             }
-
             return reverse;
         }
     }
