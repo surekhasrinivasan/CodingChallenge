3edd491968b88e12fc13de987b401991ffbd8dab
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-16 11:00:35 -0400

    IntegerReverseOrder challenge - initial commit

---
+++ IntegerReverseOrder/IntegerReverseOrder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerReverseOrder
+{
+    // create a function that will take an integer and reverse the order
+    // ex
+    // Input: 21445 Output : 54412
+    // Input: 145263 Output : 362541
+    // Input: 1254859723 Output: 3279584521
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
