a92000691581aaf0422aaa35ebb9768289383038
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-17 09:49:56 -0400

    Created class and method to find adjacentelements product

---
+++ ArcadeChallenges/AdjacentElementsProduct/AdjacentElementsProduct/AdjacentElementsProductKata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdjacentElementsProduct
+{
+    class AdjacentElementsProductKata
+    {
+        public static int adjacentElementsProduct(int[] inputArray)
+        {
+            // Code here 
+            return 0;
+        }
+    }
+}
