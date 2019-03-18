5c3bd9a118b17b86373aa06aed363136c1df17d6
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-18 17:27:39 -0400

    InstaCartChallenge - initial commit

---
+++ CompanyChallenges/InstaCartChallenge/InstaCartChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaCartChallenge
+{
+    /* Our task is to implement an algorithm that determines whether shoppers should take the given order or not.
+     * For each shopper you know their travel speed, distance to the store and the estimated amount of time they will spend there. 
+     * Figure out which of them can take the order, assuming it is known when the customer wants to receive the groceries and 
+     * the distance between their house and the store.
+     * Example
+     * For order = [200, 20, 15] and shoppers = [[300, 40, 5], [600, 40, 10]], the output should be
+     * delivery(order, shoppers) = [false, true].
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
