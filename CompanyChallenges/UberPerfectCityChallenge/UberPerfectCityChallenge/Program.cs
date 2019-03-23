d6026f2f9931f04aa582654fc21bd11f0cee8d90
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-23 16:57:47 -0400

    UberPerfectCityChallenge - initial commit

---
+++ CompanyChallenges/UberPerfectCityChallenge/UberPerfectCityChallenge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberPerfectCityChallenge
+{
+    /* Consider a city where the streets are perfectly laid out to form an infinite square grid. 
+     * In this city finding the shortest path between two given points (an origin and a destination) is much easier than in 
+     * other more complex cities. As a new Uber developer, you are tasked to create an algorithm that does this calculation.
+     * Given user's departure and destination coordinates, each of them located on some street, find the length of the shortest 
+     * route between them assuming that cars can only move along the streets. Each street can be represented as a straight line 
+     * defined by the x = n or y = n formula, where n is an integer.
+     * 
+     * Example
+     * For departure = [0.4, 1] and destination = [0.9, 3], the output should be perfectCity(departure, destination) = 2.7.
+     * 0.6 + 2 + 0.1 = 2.7, which is the answer.
+     */ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
