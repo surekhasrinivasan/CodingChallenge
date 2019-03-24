61605c1fd65a31eab0770da7187714c208a40751
surekhasrinivasan <surekhasrao@gmail.com>
2019-03-23 22:07:46 -0400

    Modified code in OrderDelivery.cs

--- CompanyChallenges/InstaCartChallenge/InstaCartChallenge/OrderDelivery.cs
+++ CompanyChallenges/InstaCartChallenge/InstaCartChallenge/OrderDelivery.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace InstaCartChallenge
 {
-    class OrderDelivery
-    {
+    public static class OrderDelivery
+    {        
+        public static bool[] Delivery(int[] order, int[][] shoppers)
+        {
+            bool[] results = new bool[shoppers.Length];
+            double order_distance = order[0];
+            double order_readyTime = order[1];
+            double order_waitTime = order[2];
 
+            for (int i = 0; i < shoppers.Length; i++)
+            {
+                double travelTime = (order_distance + shoppers[i][0]) / shoppers[i][1] + shoppers[i][2];
+
+                double orderTimeLimit = order_readyTime + order_waitTime;
+
+                if ((travelTime < order_readyTime) || (travelTime > orderTimeLimit))
+                {
+                    results[i] = false;
+                }
+                else
+                {
+                    results[i] = true;
+                }
+                Console.WriteLine(travelTime);
+                Console.WriteLine(orderTimeLimit);
+            }
+            return results;
+        }
     }
 }
